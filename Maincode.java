650d289dec588d1e35645e6f76669df81cee7485
Ronit Das <59822174+Ronit7392@users.noreply.github.com>
2022-03-05 23:14:24 +0530

    Create Maincode.java

---
+++ Maincode.java
@@ -0,0 +1,14 @@
+package com.ronit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player1 player = new Player1("ronit","sword",100);
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+
+
+    }
+
+}
